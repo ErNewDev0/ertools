da6c917504a5ab2817dc34138a404549160b4421
Just R <remixingmagelang@gmail.com>
2025-01-03 02:49:33 +0700

    update ertools/logger.py

--- ertools/logger.py
+++ ertools/logger.py
@@ -2,6 +2,7 @@ import logging
 import random
 import sys
 
+# Pilihan warna untuk membuat log lebih menarik
 COLORS = [
     "\033[1;91m",  # Merah Terang
     "\033[1;92m",  # Hijau Terang
@@ -9,22 +10,32 @@ COLORS = [
     "\033[1;94m",  # Biru Terang
     "\033[1;95m",  # Ungu Terang
     "\033[1;96m",  # Cyan Terang
-    "\033[1;97m",  # Putih Terangg
+    "\033[1;97m",  # Putih Terang
 ]
-color = random.choice(COLORS)
 
+def random_color():
+    return random.choice(COLORS)
 
 class ColorfulFormatter(logging.Formatter):
+    """
+    Formatter untuk menambahkan warna pada log.
+    """
     def format(self, record):
+        color = random_color()  # Warna acak setiap log
         message = super().format(record)
         return f"{color}{message}\033[0m"
 
-
 class LoggerHandler:
+    """
+    Kelas untuk mengatur logger dengan warna dan opsi level logging.
+    """
     def __init__(self, format_str: str = "%(asctime)s: [%(levelname)s] - %(name)s - %(message)s"):
         self.formatter = ColorfulFormatter(format_str)
 
     def setup_logger(self, error_logging: bool = False, log_level=logging.INFO):
+        """
+        Menyiapkan logger dengan handler dan formatter berwarna.
+        """
         logging.basicConfig(level=log_level, handlers=[logging.StreamHandler(sys.stdout)])
         for handler in logging.getLogger().handlers:
             handler.setFormatter(self.formatter)
@@ -34,4 +45,7 @@ class LoggerHandler:
             logging.getLogger("asyncio").setLevel(logging.CRITICAL)
 
     def get_logger(self, name: str):
+        """
+        Mengembalikan logger dengan nama tertentu.
+        """
         return logging.getLogger(name)
