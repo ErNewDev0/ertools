9d35835bf5be061fdbbf45ade40b587b9faefd6f
Just R <remixingmagelang@gmail.com>
2025-01-04 13:45:24 +0700

    update ertools/chatbot.py

--- ertools/chatbot.py
+++ ertools/chatbot.py
@@ -44,7 +44,7 @@ class Api:
             text = Handler().getMsg(message, is_chatbot=True)
             mention = Extract().getMention(message.from_user)
             chat = message.chat.tilte
-            msg = f"gue {mention}, Tolong Jawabnya Panggil {mention} ye. gw di {chat}, {text}" if message.from_user.id not in chat_history else text
+            msg = f"gue {mention}, Tolong Jawabnya Panggil {mention} ye. gw di {chat}, {text}. {chat} adalah group telegram" if message.from_user.id not in chat_history else text
 
             model = self.configure_model("chatbot")
             history = chat_history.setdefault(message.from_user.id, [])
