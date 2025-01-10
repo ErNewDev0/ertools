dc4713f1d8af525da385f3603e37178fe41724e2
Just R <remixingmagelang@gmail.com>
2025-01-10 20:21:42 +0700

    update ertools/misc.py

--- ertools/misc.py
+++ ertools/misc.py
@@ -10,31 +10,19 @@ class Handler:
         return message.text.split(None, 1)[1] if len(message.command) > 1 else ""
 
     def getMsg(self, message, is_chatbot=False):
-        reply_text = (
-            message.reply_to_message.text or message.reply_to_message.caption
-            if message.reply_to_message
-            else ""
-        )
-        user_text = (
-            message.text
-            if is_chatbot
-            else (
-                message.text.split(None, 1)[1] if len(message.text.split()) >= 2 else ""
-            )
-        )
-        return (
-            f"{user_text}\n\n{reply_text}".strip()
-            if reply_text and user_text
-            else reply_text + user_text
-        )
+        reply_text = message.reply_to_message.text or message.reply_to_message.caption if message.reply_to_message else ""
+        user_text = message.text if is_chatbot else (message.text.split(None, 1)[1] if len(message.text.split()) >= 2 else "")
+        return f"{user_text}\n\n{reply_text}".strip() if reply_text and user_text else reply_text + user_text
 
-    async def kirim_file(self, message, output):
+    async def kirim_file(self, message, output, is_delete=None):
         if len(output) <= 4000:
             await message.reply(output)
         else:
             with BytesIO(output.encode()) as out_file:
                 out_file.name = "result.txt"
                 await message.reply_document(document=out_file)
+        if is_delete:
+            await is_delete.delete()
 
     async def getTime(self, seconds):
         time_units = [(60, "s"), (60, "m"), (24, "h"), (7, "d"), (4.34812, "w")]
